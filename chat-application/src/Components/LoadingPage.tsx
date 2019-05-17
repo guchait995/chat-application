3eedfff6375075a774a1934a459593a19d3bee03
guchait995 <46808879+sourav-witbybit@users.noreply.github.com>
2019-05-17 11:46:05 +0530

    ui fixes

--- chat-application/src/Components/LoadingPage.tsx
+++ chat-application/src/Components/LoadingPage.tsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function LoadingPage(props) {
   return (
     <div className="full-page" hidden={props.hidden}>
-      <div className="centre">
-        <div className="loading-rotate" />
-      </div>
+      <div className="loading-rotate" />
     </div>
   );
 }
